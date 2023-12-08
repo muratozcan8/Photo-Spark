78cac982bc4e17a29619499dd3872bdcb36b30a1
muratozcan8 <mrtzcn08@gmail.com>
2023-12-08 20:36:43 +0300

    Added login and sign up operations

--- app/src/main/java/com/muratozcan/instaclone/view/LoginFragment.kt
+++ app/src/main/java/com/muratozcan/instaclone/view/LoginFragment.kt
@@ -5,13 +5,55 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.navigation.fragment.findNavController
+import com.google.firebase.Firebase
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.auth
 import com.muratozcan.instaclone.R
+import com.muratozcan.instaclone.databinding.FragmentLoginBinding
 
 class LoginFragment : Fragment() {
 
+    private lateinit var binding: FragmentLoginBinding
+    private lateinit var auth: FirebaseAuth
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_login, container, false)
+        binding = FragmentLoginBinding.inflate(inflater, container, false)
+        auth = Firebase.auth
+        val navController = findNavController()
+
+        val currentUser = auth.currentUser
+        if (currentUser != null) {
+            //Eğer kullanıcı daha önceden giriş yaptıysa otomatik olarak homepage'e geçer
+            //navController.navigate(R.id.action_loginFragment_to_homePageFragment)
+        }
+
+        binding.buttonSignIn.setOnClickListener{
+            signIn()
+        }
+
+        binding.textViewSignUp.setOnClickListener {
+            navController.navigate(R.id.action_loginFragment_to_signUpFragment)
+        }
+
+        return binding.root
+    }
+
+    private fun signIn(){
+        val email = binding.editTextEmailSignIn.text.toString()
+        val password = binding.editTextPasswordSignIn.text.toString()
+
+        if (email == "" || password == "") {
+            Toast.makeText(this.context, "Enter email and password!", Toast.LENGTH_LONG).show()
+        } else {
+            auth.signInWithEmailAndPassword(email, password)
+                .addOnSuccessListener {
+                    val navController = findNavController()
+                    navController.navigate(R.id.action_loginFragment_to_homePageFragment)
+                }.addOnFailureListener {
+                    Toast.makeText(this.context, it.localizedMessage, Toast.LENGTH_LONG).show()
+                }
+        }
     }
 
 }
--- app/src/main/java/com/muratozcan/instaclone/view/SignUpFragment.kt
+++ app/src/main/java/com/muratozcan/instaclone/view/SignUpFragment.kt
@@ -1,20 +1,51 @@
 package com.muratozcan.instaclone.view
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.navigation.fragment.findNavController
+import com.google.firebase.Firebase
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.auth
 import com.muratozcan.instaclone.R
+import com.muratozcan.instaclone.databinding.FragmentSignUpBinding
 
 
 class SignUpFragment : Fragment() {
 
-
+    private lateinit var binding: FragmentSignUpBinding
+    private lateinit var auth: FirebaseAuth
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_sign_up, container, false)
+        binding = FragmentSignUpBinding.inflate(inflater, container, false)
+        auth = Firebase.auth
+
+        binding.button.setOnClickListener {
+            singUp()
+        }
+
+        return binding.root
     }
 
+    private fun singUp(){
+
+        val email = binding.editTextEmailSignUp.text.toString()
+        val password = binding.editTextPasswordSignUp.text.toString()
+
+        if (email == "" || password == "") {
+            Toast.makeText(this.context, "Enter email and password!", Toast.LENGTH_LONG).show()
+        } else {
+            auth.createUserWithEmailAndPassword(email, password)
+                .addOnSuccessListener {
+                    val navController = findNavController()
+                    navController.navigate(R.id.action_signUpFragment_to_loginFragment)
+                }.addOnFailureListener {
+                    Toast.makeText(this.context, it.localizedMessage, Toast.LENGTH_LONG).show()
+                }
+        }
+    }
 
 }
