2d63d8766c8a819eb5fd492b1c29d94ba7ddc889
muratozcan8 <mrtzcn08@gmail.com>
2023-12-09 02:32:35 +0300

    Designed Login Page

--- app/src/main/java/com/muratozcan/instaclone/view/AuthActivity.kt
+++ app/src/main/java/com/muratozcan/instaclone/view/AuthActivity.kt
@@ -3,10 +3,15 @@ package com.muratozcan.instaclone.view
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.muratozcan.instaclone.R
+import com.muratozcan.instaclone.databinding.ActivityAuthBinding
+import com.muratozcan.instaclone.databinding.ActivityMainBinding
 
 class AuthActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityAuthBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_auth)
+        binding = ActivityAuthBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
